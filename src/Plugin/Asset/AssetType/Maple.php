53512fa4e8c04903dafc5c03ddd482df114a4bea
Michael Stenta <mike@mstenta.net>
2022-07-14 13:29:12 -0400

    Add a "Maple" asset type.

---
+++ src/Plugin/Asset/AssetType/Maple.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_maple\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the maple asset type.
+ *
+ * @AssetType(
+ *   id = "maple",
+ *   label = @Translation("Maple"),
+ * )
+ */
+class Maple extends FarmAssetType {
+
+}
