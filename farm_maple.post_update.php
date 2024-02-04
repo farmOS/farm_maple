4f0d959eb35a9f066f8b0de5c474511c7845661e
Michael Stenta <mike@mstenta.net>
2024-02-04 08:13:05 -0500

    Fix Error: Class "Drupal\migrate_plus\Entity\Migration" not found
    This happens if the migrate_plus module is not installed when
    the update hook runs.

--- farm_maple.post_update.php
+++ farm_maple.post_update.php
@@ -5,17 +5,15 @@
  * Post update hooks for the farm_maple module.
  */
 
-use Drupal\migrate_plus\Entity\Migration;
-
 /**
  * Uninstall v1 migration configs.
  */
 function farm_maple_post_update_uninstall_v1_migrations(&$sandbox) {
-  $config = Migration::load('farm_migrate_asset_maple');
+  $config = \Drupal::configFactory()->getEditable('migrate_plus.migration.farm_migrate_asset_maple');
   if (!empty($config)) {
     $config->delete();
   }
-  $config = Migration::load('farm_migrate_log_tap');
+  $config = \Drupal::configFactory()->getEditable('migrate_plus.migration.farm_migrate_log_tap');
   if (!empty($config)) {
     $config->delete();
   }
